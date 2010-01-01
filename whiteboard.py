07036f3fcf0f81c4b5709b93ce59a1c99904a34b
vr96 <varunraghav96@gmail.com>
2009-12-31 19:09:14 -0500

    Added the code for LEDs, Vibration Sensors, Thermistors and sound sensors.

--- whiteboard.py
+++ whiteboard.py
@@ -2,16 +2,44 @@ from gpio import *
 import time
 
 def pressureSensor():
-	#top , bottom, seat =2,3,4
-	top = 2	
+	top , bottom, seat =2,3,0
 	topPressure = analogRead(top)
-	#bottomPressure = analogRead(bottom)
-	#seatPressure = analogRead(seat)
+	bottomPressure = analogRead(bottom)
+	seatPressure = analogRead(seat)
 	
-	#return (topPressure, bottomPressure, seatPressure)
-	return topPressure
+	return (topPressure, bottomPressure, seatPressure)
+
+def blinkLed():
+	topPressure, bottomPressure, seatPressure = pressureSensor()
+	# top = 200 to 500 
+	# bottom = 100 to 400
+	# seat = 10 to 40
+	ledGreen = 1
+	if (topPressure >= 250 and topPressure <= 500) and (bottomPressure >= 100 and bottomPressure <= 400) and (seatPressure >10):
+		pinMode(1,OUTPUT)	
+		digitalWrite(ledGreen, HIGH)
+
+def vibrationSensor():
+	vibrationPin = 4
+	pinMode(4, OUTPUT)
+	digitalWrite(vibrationPin, HIGH)
+ 	
+def soundSensor():
+	soundPin = 5
+	soundValue = analogRead(soundPin)
+	return soundValue
+
+def thermistorSensor():
+	thermPin = 4
+	thermVal = analogRead(thermPin)
+	return thermVal
 
 while(True):
-	print pressureSensor()
+	#blinkLed()
+	#print pressureSensor()
+	vibrationSensor()
+	#print soundSensor()
+	#print thermistorSensor()
 	time.sleep(1)
+	
  
