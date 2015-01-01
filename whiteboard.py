89edffb4759578392f1e4e5cb20fd49fc85109ae
vr96 <varunraghav96@gmail.com>
2015-01-01 00:09:09 -0500

    Testing Pressure Sensors

--- whiteboard.py
+++ whiteboard.py
@@ -1,12 +1,16 @@
 from gpio import *
+import time
 
 def pressureSensor():
-	top , bottom, seat =2,3,4
+	#top , bottom, seat =2,3,4
+	top = 2	
 	topPressure = analogRead(top)
-	bottomPressure = analogRead(bottom)
-	seatPressure = analogRead(seat)
+	#bottomPressure = analogRead(bottom)
+	#seatPressure = analogRead(seat)
 	
-	return (topPressure, bottomPressure, seatPressure)
+	#return (topPressure, bottomPressure, seatPressure)
+	return topPressure
 
-
-
+while(True):
+	print pressureSensor()
+	time.sleep(1)
