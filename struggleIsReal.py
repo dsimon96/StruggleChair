4d1ee9a15fd496c71964f5a31e7177ebba2511f0
Rajat Mehndiratta <rajatmehndiratta@cmu.edu>
2015-01-12 21:45:10 -0500

    Whoops! Wrong file earlier!

--- struggleIsReal.py
+++ struggleIsReal.py
@@ -1,2 +1,30 @@
 # STRUGGLE CHAIR -- BUILD18 JAN 2015
-# 
+# Built and coded by rmehndir, vrramesh, dasimon, and sgakhar
+# CMU ECE c/ 2018
+
+class StruggleChair(object):
+
+	def __init__(self, topPin, bottomPin, lbServo,
+				 rbServo, ltServo, rtServo):
+		self.topPin, self.bottomPin = topPin, bottomPin
+		 # pins for pressure clusters
+		self.servoPins = [lbServo, rbServo, ltServo, rtServo]
+		# left bottom, right bottom, left top, right top
+
+	def read(self):
+		# calls all sensors and adjusts variables
+
+	def pressure(self):
+		# get pressure from top cluster
+		# get pressure from bottom cluster
+		# stores output in self.topPressure and self.bottomPressure
+
+	def parse(self):
+		# checks sensor variables
+		# adjusts accordingly
+
+	def moveTop(self):
+		# uses servos to move chair's back (top)
+
+	def moveBottom(self):
+		# uses servos to move chair's back (bottom)
